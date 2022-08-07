749e39455eabd00fb5cac9afbe355343946c6161
shubham <patilshubham2k21@gmail.com>
2022-08-07 18:57:11 +0530

    add User Login Reducer & Action in frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,27 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { composedWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer = combineReducers({
-
+    userLogin: userLoginReducer,
 })
 
-const initialState = {
+// load userInfo from localStorage if present
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
+
+const initialState = {
+    userLogin: { userInfo: userInfoFromStorage},
 }
 
 const store = createStore(
     reducer,
     initialState,
-    composedWithDevTools(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(thunk))
 )
+
+export default store
